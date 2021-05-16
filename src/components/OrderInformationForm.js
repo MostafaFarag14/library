0cea9ac3f8e938cbec87dee9a69e20a8bf71c996
MostafaFarag14 <mostafafarag140@gmail.com>
2021-05-16 17:19:07 +0200

    refactor order info with formik

--- src/components/OrderInformationForm.js
+++ src/components/OrderInformationForm.js
@@ -1,20 +1,82 @@
-import React from 'react'
-import { Grid, Input } from 'semantic-ui-react'
+import { ErrorMessage, Field, Form, Formik } from 'formik'
+import * as Yup from 'yup'
+import React, { useContext } from 'react'
+import { Button, Grid, Input } from 'semantic-ui-react'
 import { removeSpaces } from '../api/helpers'
+import styled from 'styled-components'
+import { CartContext } from '../contexts/CartContext'
+import { getAllProducts } from '../api/helpers'
 
-export default function OrderInformationForm({ getFieldProps, errors, touched }) {
+const StyledErrorMessage = styled.div`
+display: inline-block;
+color: red;
+`
+const StyledLabel = styled.label`
+display: inline-block;
+margin: 5px;
+`
+
+const initialValues = {
+  firstname: '',
+  lastname: '',
+  email: '',
+  phonenumber: '',
+  address: ''
+}
+const validationSchema = Yup.object().shape({
+  firstname: Yup.string().required('Required'),
+  lastname: Yup.string().required('Required'),
+  email: Yup.string().email('Invalid Email Format').required('Required'),
+  phonenumber: Yup.string().length(11, 'Invalid Phone Number').matches(/[0-9]/, 'Invalid Phone Number').required('Required'),
+  address: Yup.string().required('Required')
+})
+
+
+export default function OrderInformationForm({ setOrderDetails, setActiveStep }) {
+  const { cart } = useContext(CartContext)
+  const onSubmit = async values => {
+    const { items = [] } = cart
+    const productsIDs = items.map(product => `id_in=${product.id}`)
+    const query = productsIDs.join('&')
+    let total = 0
+    const products = await getAllProducts(query)
+    products.forEach(product => {
+      const item = items.find(item => item.id === product.id)
+      total += product.price * item.quantity
+    })
+    setOrderDetails({ ...values, total: `${total}` })
+    setActiveStep(1)
+  }
+
   return (
-    <Grid padded>
-      {['First Name', 'Last Name', 'Email', 'Phone Number'].map((item, index) => {
-        const name = removeSpaces(item).toLowerCase()
-        return (<Grid.Column key={index} computer={8} mobile={16} >
-          <Input fluid name={name} placeholder={item} {...getFieldProps(name)} error={touched[name]}/>
-        </Grid.Column>)
-      }
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      {({ errors, touched }) => (
+        <Form>
+          <Grid padded>
+            {['First Name', 'Last Name', 'Email', 'Phone Number'].map((item, index) => {
+              const name = removeSpaces(item).toLowerCase()
+              return (<Grid.Column key={index} computer={8} mobile={16}>
+                <StyledLabel>{item}</StyledLabel>
+                <ErrorMessage name={name} component={StyledErrorMessage} />
+                <Field as={Input} type='text' fluid name={name} placeholder={item} error={errors[name] && touched[name]} />
+              </Grid.Column>)
+            }
+            )}
+            <Grid.Column width={16}>
+              <StyledLabel>Address</StyledLabel>
+              <ErrorMessage name='address' component={StyledErrorMessage} />
+              <Field as={Input} type='text' fluid placeholder='Address' name='address' error={errors.address && touched.address} />
+            </Grid.Column>
+          </Grid>
+          <Button type='submit' floated='right' disabled={cart.total <= 0 || cart.items.length === 0}
+            color='vk' content='Continue To Payment'
+          />
+        </Form>
       )}
-      <Grid.Column width={16}>
-        <Input fluid placeholder='Address' name='address' {...getFieldProps('address')} error={touched['address']}/>
-      </Grid.Column>
-    </Grid>
+    </Formik>
   )
 }
--- src/pages/CheckOut.js
+++ src/pages/CheckOut.js
@@ -1,11 +1,9 @@
-import { useFormik, yupToFormErrors } from 'formik'
 import React, { useContext, useState } from 'react'
 import { Button, Header } from 'semantic-ui-react'
 import CartDetail from '../components/CartDetail'
 import OrderInformationForm from '../components/OrderInformationForm'
-import * as Yup from 'yup'
 import { CartContext } from '../contexts/CartContext'
-import { addOrder, getAllProducts } from '../api/helpers'
+import { addOrder } from '../api/helpers'
 import { useHistory } from 'react-router-dom'
 import CompletionSteps from '../components/CompletionSteps'
 import PayButton from '../components/PayButton'
@@ -17,49 +15,14 @@ export default function CheckOut() {
   const [payOnline, setPayOnline] = useState(true)
   const [orderDetails, setOrderDetails] = useState()
   const history = useHistory()
-  const formik = useFormik({
-    initialValues: {
-      firstname: '',
-      lastname: '',
-      email: '',
-      phonenumber: '',
-      address: ''
-    },
-    validationSchema: Yup.object().shape({
-      firstname: Yup.string().required(),
-      lastname: Yup.string().required(),
-      email: Yup.string().email().required()
-    }),
-    onSubmit: async values => {
-      console.log('gg')
-      const { items = [] } = cart
-      const productsIDs = items.map(product => `id_in=${product.id}`)
-      const query = productsIDs.join('&')
-      let total = 0
-      const products = await getAllProducts(query)
-      products.forEach(product => {
-        const item = items.find(item => item.id === product.id)
-        total += product.price * item.quantity
-      })
-      setOrderDetails({ ...values, total: `${total}` })
-      setActiveStep(1)
 
-    }
-  })
-
-  const { getFieldProps, errors, touched } = formik
   return (
-    <div style={{margin: '20px 0px' }}>
+    <div style={{ margin: '20px 0px' }}>
       <CompletionSteps activeStep={activeStep} />
-      <CartDetail/>
+      <CartDetail />
       {
         activeStep === 0 ?
-          <>
-            <OrderInformationForm getFieldProps={getFieldProps} errors={errors} touched={touched} />
-            <Button floated='right' disabled={cart.total <= 0 || cart.items.length === 0}
-              color='vk' content='Continue To Payment' onClick={formik.handleSubmit}
-            />
-          </>
+          <OrderInformationForm setActiveStep={setActiveStep} setOrderDetails={setOrderDetails}/>
           :
           activeStep === 1 ?
             <>
@@ -75,9 +38,9 @@ export default function CheckOut() {
               <Header content='Order Confirmation' />
               <Button floated='right' disabled={cart.total <= 0 || cart.items.length === 0}
                 color='vk' content='Place Order' onClick={async () => {
-                const order = await addOrder(orderDetails)
-                history.push(`/orders/${order.code}`)
-              }} />
+                  const order = await addOrder(orderDetails)
+                  history.push(`/orders/${order.code}`)
+                }} />
             </>
       }
 
